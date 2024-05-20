b2f9501f85cbaeaa4b79c51a507040d8c4515327
GD Radloff <danielrad082@gmail.com>
2024-05-20 13:40:15 +0200

    Moved firebase state into onmount event

--- code/apps/web/src/routes/+layout.svelte
+++ code/apps/web/src/routes/+layout.svelte
@@ -4,38 +4,40 @@
   import { onAuthStateChanged } from "firebase/auth";
 	import { userAuthInfo, userDegrees, userMetadata } from "../stores";
 	import { get } from "svelte/store";
-	import { onDestroy } from "svelte";
+	import { onDestroy, onMount } from "svelte";
   import { Collections, userMetadata as userMetadataValidate } from '@cos720project/shared';
 	import { doc, getDoc } from "firebase/firestore";
 
-  const auth = getFirebaseAuthClient();
-  onAuthStateChanged(auth,(user) => {
-    if (user) {
-      // signed in
-      userAuthInfo.set(user);
-      // get metadata
-      if (get(userMetadata) == undefined) {
-        const firestore = getFirebaseFirestoreClient();
-        const clear = setInterval(async ()=>{
-          try {
-            const userId = get(userAuthInfo)?.uid;
-            const userMetadataDocReference = doc(firestore,Collections.metadata,userId ? userId : "default")
-            const userMetadataSnap = await getDoc(userMetadataDocReference);
-            if (userMetadataSnap.exists()) {
-              userMetadata.set(userMetadataValidate.parse(userMetadataSnap.data()))
-              clearInterval(clear);
+  onMount(() => {
+    const auth = getFirebaseAuthClient();
+    onAuthStateChanged(auth,(user) => {
+      if (user) {
+        // signed in
+        userAuthInfo.set(user);
+        // get metadata
+        if (get(userMetadata) == undefined) {
+          const firestore = getFirebaseFirestoreClient();
+          const clear = setInterval(async ()=>{
+            try {
+              const userId = get(userAuthInfo)?.uid;
+              const userMetadataDocReference = doc(firestore,Collections.metadata,userId ? userId : "default")
+              const userMetadataSnap = await getDoc(userMetadataDocReference);
+              if (userMetadataSnap.exists()) {
+                userMetadata.set(userMetadataValidate.parse(userMetadataSnap.data()))
+                clearInterval(clear);
+              }
+            } catch (error) {
+              console.log(error);
             }
-          } catch (error) {
-            console.log(error);
-          }
-        },1000)
+          },1000)
+        }
+      } else {
+        // logged out
+        userAuthInfo.set(undefined);
+        userMetadata.set(undefined);
+        userDegrees.set(undefined);
       }
-    } else {
-      // logged out
-      userAuthInfo.set(undefined);
-      userMetadata.set(undefined);
-      userDegrees.set(undefined);
-    }
+    })
   })
 </script>
 
